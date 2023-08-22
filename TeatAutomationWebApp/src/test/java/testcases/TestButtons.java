99c22536ea6c596cc768092a2c5b8f7ce18ed165
Revanth-25 <142740859+Revanth-25@users.noreply.github.com>
2023-08-22 23:53:11 +0530

    Update TestButtons.java

--- TeatAutomationWebApp/src/test/java/testcases/TestButtons.java
+++ TeatAutomationWebApp/src/test/java/testcases/TestButtons.java
@@ -9,7 +9,7 @@ public class TestButtons extends baseTest {
 	
 	@Test
 	public static void testButton() throws Exception {
-		Thread.sleep(5000);
+		Thread.sleep(3000);
 		
 		driver.findElement(By.id("add")).click();
 		Thread.sleep(3000);
