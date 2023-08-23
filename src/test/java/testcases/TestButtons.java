c52a2813be0d3130b86ada90c9f99576e77faa17
Revanth-25 <142740859+Revanth-25@users.noreply.github.com>
2023-08-23 13:55:39 +0530

    Update TestButtons.java

--- src/test/java/testcases/TestButtons.java
+++ src/test/java/testcases/TestButtons.java
@@ -9,10 +9,10 @@ public class TestButtons extends baseTest {
 	
 	@Test
 	public static void testButton() throws Exception {
-		Thread.sleep(4000);
+		Thread.sleep(2000);
 		
 		driver.findElement(By.xpath("//*[@id='add']")).click();
-		Thread.sleep(3000);
+		Thread.sleep(2000);
 		driver.findElement(By.xpath("//*[@id='order']")).click();
 		Thread.sleep(2000);
 	}
