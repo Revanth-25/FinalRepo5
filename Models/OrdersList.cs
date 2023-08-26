0c5c441179179b9d1ad4197cec85d04a44e2476f
Revanth-25 <142740859+Revanth-25@users.noreply.github.com>
2023-08-26 16:39:25 +0530

    Update OrdersList.cs

--- Models/OrdersList.cs
+++ Models/OrdersList.cs
@@ -6,12 +6,12 @@ namespace OrdersListWeb.Models
     public class OrdersList
     {
         [Key]
-        public int Id { get; set; }
+        public int? Id { get; set; }
         [Required]
-        public string Name { get; set; }
+        public string? Name { get; set; }
         [DisplayName("Order Number")]
         [Range(1,10000,ErrorMessage ="Enter a valid Order Id!")]
-        public int OrderNumber { get; set; }
+        public int? OrderNumber { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
 }
